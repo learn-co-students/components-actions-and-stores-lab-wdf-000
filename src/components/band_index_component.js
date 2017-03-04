4745e3e6abb0e24edada416d11da6f1fa808e791
blessedsoy <unmigong@gmail.com>
2017-03-03 21:58:22 -0500

    Done.

---
+++ src/components/band_index_component.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+ 
+// const BandIndex = (props) => {
+//   return(
+//     <li>
+//       {props.band.title}
+//     </li>
+//   )
+// }
+ 
+// export default BandIndex;
+
+
+import React from 'react';
+
+const BandIndex = (props) => {
+  return(
+    <li>
+      {props.band.title}
+    </li>
+  )
+}
+
+export default BandIndex;
--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,8 +1,74 @@
-import React, { Component } from 'react'
-class BandInputComponent extends Component {
-  render(
+// import React from 'react';
+// import BandIndex  from './band_index_component';
+ 
+// export default class BandInput extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.onSubmit = this.onSubmit.bind(this)
+//   }
+ 
+//   onSubmit(event){
+//     event.preventDefault();
+//     this.props.store.dispatch({type: 'ADD_BAND', payload: {title: event.target.children[1].value}})
+//     event.target.children[1].value = ''
+// }
+ 
+//   render(){
+//     return(
+//       <div>
+//         <form onSubmit={this.onSubmit}>
+//           <label>Enter Band: </label>
+//           <input />
+//           <button type="submit">Submit</button>
+//         </form>
+//         <h1>Bands</h1>
+//         <ul>
+//             {this.props.store.getState().map((band, i) => <BandIndex key={i} band={band} />)}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+
+import React from 'react';
+import BandIndex  from './band_index_component';
+
+export default class BandInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  handleBandShow(){
+    return this.props.store !== undefined ? this.props.store.getState().map((band, i) => <BandIndex key={i} band={band} />) : undefined
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    console.log(this.props.store.getState())
+    this.props.store.dispatch({type: 'ADD_BAND', payload: {title: event.target.children[1].value}})
+    event.target.children[1].value = ''
+  }
+
+  render(){
     return(
-      <div>hello</div>
+      <div>
+        <br/>
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Enter Band: </label>
+          <input />
+          <button type="submit">Submit</button>
+        </form>
+
+        <h1>Bands</h1>
+        <ul>
+          {this.handleBandShow()}
+        </ul>
+
+
+      </div>
     )
-  )
+  }
 }
